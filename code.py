ad630cae409e66ff77146d07f8733168d0a607cd
Swetha <82767077+Swetha2911@users.noreply.github.com>
2024-04-05 20:54:03 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from mysql.connector import Error 
 
-mydb=mysql.connector.connect(
+mydb = mysql.connector.connect(
     host="127.0.0.1",
     user="root",
     password="root",
@@ -9,68 +9,57 @@ mydb=mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
-print("Welcome to atm")
-n=int(input("Press 1 for login and 0 for register"))
+print("Welcome to ATM")
+n = int(input("Press 1 for login and 0 for register: "))
 
-password=""
-myname=""
-depo=""
-id=""
+if n == 0:
+    name = input("Enter name: ")
+    accn = int(input("Enter account no: "))
+    deposit = int(input("Enter Deposit Amount: "))
+    pw = input("Set Your Password: ")
 
-if n==0:
-    name=input("Enter name: ")
-    accn=int(input("Enter account no: "))
-    t=int(input("Enter Deposit Amount"))
-    pw=int(input("Set Your Password:"))
+    val = (name, pw, deposit, accn) 
+    sql = """INSERT INTO user_data (my_name, pass_word, de_po, accn) 
+             VALUES (%s, %s, %s, %s)"""
 
-    myname=name
-    password=pw
-    deposit=t
-    id=accn
-
-    val=(myname,password,depo,id)
-
-    sql= """insert into user_data(my_name, pass_word, de_po, i_d) 
-           values(%s,%s,%s,%s) """
-
-    mycursor.execute(sql,val)
+    mycursor.execute(sql, val)
     mydb.commit()
-    print("successfully inserted data to table")
+    print("Account created succesfully!!!")
 
-if n==1:
-    info= int(input("Enter ur id: "))
-    passw=int(input("Enter the password: "))
-    mycursor.execute("select * from atm.user_data where i_d=%s"%(info)) 
-    row=mycursor.fetchone()
+if n == 1:
+    info = int(input("Enter your account number: "))
+    passw = input("Enter the password: ")
 
-    if mycursor.rowcount==1:
-        mycursor.execute("select * from atm.user_data where pass_word= %s"%(passw))
-        row=mycursor.fetchone()
+    # Execute the first query to check if the account exists and the password is correct
+    mycursor.execute("SELECT * FROM atm.user_data WHERE accn = %s AND pass_word = %s", (info, passw))
+    row = mycursor.fetchone()
 
-        if mycursor.rowcount==1:
-            print("login successful")
-            d=int(input("Enter 1 for withdrawl or 0 for deposit and 3 for exit: "))
+    if row:
+        print("Login successful")
+        d = int(input("Enter 1 for withdrawal, 0 for deposit, and 2 for exit: "))
 
-            if d==1:
-                a= int(input("How much you want to withdrawl?: "))
-                print(a)   
-                mycursor.execute("select de_po from atm.user_data where pass_word=%s" %(passw))
-                col=mycursor.fetchone()
-                x=list(col)
-
-                for i in x:
-                    z=(int(i))
-                    c=z-a
-                    print(c)   
-                mycursor.execute("update atm.user_data set de_po=%s where i_d=%s" %(c,info))
-
-            if d==3:
-                exit(0)
-
-        else:
-            print("invalid password")
-
+        if d == 1:
+            withdrawal_amount = int(input("Enter the withdrawal amount: "))
+            current_balance = row[3]
+            if current_balance >= withdrawal_amount:
+                new_balance = current_balance - withdrawal_amount
+                mycursor.execute("UPDATE atm.user_data SET de_po = %s WHERE accn = %s", (new_balance, info))
+                mydb.commit()
+                print("Withdrawal successful")
+                print("Remaining balance:", new_balance)
+            else:
+                print("Insufficient funds for withdrawal")
+        elif d == 0:
+            deposit_amount = int(input("Enter the deposit amount: "))
+            new_balance = row[3] + deposit_amount
+            mycursor.execute("UPDATE atm.user_data SET de_po = %s WHERE accn = %s", (new_balance, info))
+            mydb.commit()
+            print("Deposit successful")
+            print("Remaining balance:", new_balance)
+        elif d == 2:
+            exit(0)
     else:
-        print("Account does't exist") 
+        print("Invalid account number or password")
 
-    mydb.commit() 
+    
+    mydb.commit()
